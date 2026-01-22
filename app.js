f45ddf16470356e91612b4b40b5abc9d09653b44
cybill-501 <eshanpv9@gmail.com>
2026-01-22 13:18:46 +0530

    Update app.js
    adding port changes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3030;
+
 app.get('/login', (req, res) => {
-  const user = req.query.user;
+  const user = req.query.user || '';
   res.send("Hello " + user);
 });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App running on port ${PORT}`);
 });
